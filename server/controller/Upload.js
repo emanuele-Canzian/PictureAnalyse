9293776d33ebf87a2fae8e5df7d8cd488707706f
emanuele <emanuele.canzian@csbe.ch>
2020-03-24 13:00:22 +0100

    front end styling

--- server/controller/Upload.js
+++ server/controller/Upload.js
@@ -1,47 +1,66 @@
 const vision = require('@google-cloud/vision');
-const fs = require('fs');
 const multer = require('multer');
-
 
 
 //set Storage
 const storage = multer.diskStorage({
   destination: '../images/',
-  filename: function (req,file,cb) {
-    cb(null,file.fieldname + '-' + Date.now() +
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() +
       path.extname(file.originalname));
   }
 });
 const upload = multer({
-  storage : storage
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
 }).single('pictureField');
 
-module.exports={
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
 
-  async upload(req, res){
-    upload(req,res,(err) =>{
-      if (err){
-        console.log("u failed")
-      }
-      else{
-        console.log(req.file);
-        imageDir = `${process.cwd()}/images/`;
-        const client = new vision.ImageAnnotatorClient({
-          keyFilename: './ApiKey.json'
-        });
-        client
-          .labelDetection(`${imageDir}${req.file.filename}`)
-          .then(results => {
-            const labels = results[0].labelAnnotations;
-            labels.forEach(label => console.log(label));
-            res.status(200).json({data: labels});
-          })
-          .catch(err => {
-            console.error('ERROR:', err);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+module.exports = {
+
+  async upload(req, res) {
+    upload(req, res, (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        if (req.file == undefined) {
+          res.render('index', {
+            msg: 'Error: no file Selected'
           });
+        } else {
+          console.log(req.file);
+          imageDir = `${process.cwd()}/images/`;
+          const client = new vision.ImageAnnotatorClient({
+            keyFilename: './ApiKey.json'
+          });
+          client
+            .labelDetection(`${imageDir}${req.file.filename}`)
+            .then(results => {
+              const labels = results[0].labelAnnotations;
+              labels.forEach(label => console.log(label));
+              res.status(200).json({data: labels});
+            })
+            .catch(err => {
+              console.error('ERROR:', err);
+            });
+        }
       }
     });
-
   }
-
 };
